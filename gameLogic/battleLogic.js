38ab21fcad10bb9b068d415d80c8c6af61d43691
Trevor Li <lili-rovert@hotmail.com>
2018-10-30 21:20:01 -0600

    add some game logic

---
+++ gameLogic/battleLogic.js
@@ -0,0 +1,24 @@
+// Var ===================================================================
+// get planet
+// get player
+
+
+// End of Vars
+
+// Function calls ========================================================
+
+
+// End of fucntion calls
+
+// Function declaration ==================================================
+module.exports = {
+    
+}
+
+
+// End Function declarations
+
+// Anything else =========================================================
+
+
+// End Anything else
---
+++ gameLogic/playerLogic.js
@@ -0,0 +1,11 @@
+module.exports = {
+    Player: function(userName, avatar, score, attk, def, hp, level){
+        this.userName = userName,
+        this.avatar = avatar,
+        this.score = score,
+        this.attk = attk,
+        this.def = def,
+        this.hp = hp,
+        this.level = level
+    }
+};
