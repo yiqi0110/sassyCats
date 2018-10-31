cfee314eb35e7e9b6025c01ddec91d34015affaa
Matt Auretta <mauretta2@gmail.com>
2018-10-30 19:20:09 -0600

    added scores table

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,24 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/planets", function(req, res) {
-    db.Planets.findAll({}).then(function(dbPlanets) {
+module.exports = function (app) {
+  // Get all planets
+  app.get("/api/planets", function (req, res) {
+    db.Planets.findAll({}).then(function (dbPlanets) {
       res.json(dbPlanets);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // Get all users and scores
+  app.get("/api/scores", function (req, res) {
+    db.Scores.findAll({}).then(function (dbScores) {
+      res.json(dbScores);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  // Create a new example
+  app.post("/api/scores", function (req, res) {
+    db.Scores.create(req.body).then(function (dbScores) {
+      res.json(dbScores);
     });
   });
 };
