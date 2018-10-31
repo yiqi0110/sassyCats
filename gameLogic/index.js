3294a8d8a096e8bba9a6d0f4571627e0771efecf
Trevor Li <lili-rovert@hotmail.com>
2018-10-31 15:23:38 -0600

    have a portion of the battle logic complete

--- gameLogic/index.js
+++ gameLogic/index.js
@@ -1,5 +1,50 @@
-var Player = require("./playerLogic");  // instance of the player constructor
+// Var ===================================================================
+var Player = require("./playerLogic");  // also takes stuff in from the db? to be inplemented into the Player constructor
+var ship = require("./shipLogic");  
+var battle = require("./battleLogic");
+
+var goodEnding = {
+    stuff: "Good Ending achieved. I'm almost proud of you."
+};
+
+var retireEnding = {
+    stuff: "You retired swine . . . I'm a bit hurt. Or that's what I would have said had been sad you left, but I'm not."
+};
+
+var badEnding = {
+    stuff: "You died . . . swine, try harder."
+};
+// These will have other info and such later
 
 module.exports = {
 
-}
+    checkIfDead: function () {
+        if (player.hp <= 0) {
+            console.log("death");
+            console.log(badEnding.stuff);
+            // go back to start screen
+        } else {
+            // return to game
+        }
+    },
+    checkIfRetire: function(){
+        if(wish2retire){
+            ship.giveUp(percent);
+        } else {
+            // return to game
+        }
+    },
+    checkIfConquer: function(){
+        if(wish2conquer){
+            ship.conquer();
+        } else {
+            // return to ship
+        }
+    }
+    
+    
+    
+    
+    
+};
+    
