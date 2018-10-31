c33bad7ed18c119bdb3bad3dae9120eb23458dbb
Matt Auretta <mauretta2@gmail.com>
2018-10-30 21:22:10 -0600

    added api scores update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -24,4 +24,20 @@ module.exports = function (app) {
       res.json(results);
     });
   });
+
+  // PUT route for updating posts
+  app.put("/api/scores", function (req, res) {
+    // Add code here to update a post using the values in req.body, where the id is equal to
+    // req.body.id and return the result to the user using res.json
+    db.Scores.update({
+      player_name: req.body.player_name,
+      player_score: req.body.player_score
+    }, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function (results) {
+      res.json(results);
+    })
+  });
 };
