ca3d2db329277eca43edfe2c09f40c316103a15f
Tim Lewis <timothydlewis@icloud.com>
2018-11-07 20:52:21 -0700

    Update ending.js

--- public/js/ending.js
+++ public/js/ending.js
@@ -1,33 +1,6 @@
 $(document).ready(function () {
-    var outcome = ["dead"];
     var temp = sessionStorage.getItem("player");
     var newPlayer = $.parseJSON(temp);
-
-    // Set ending message
-    // Dead
-    if (outcome[0] === "dead") {
-        $("#gameEndResult").text("You Died!");
-        $("#gameEndMessage").text("As expected, but good job living up to expectations");
-        $("#gameEnd").css("background-image", "url(images/dead.png)");
-
-        // Enslaved
-    } else if (outcome[0] === "enslaved") {
-        $("#gameEndResult").text("You died a slave!");
-        $("#gameEndMessage").text("I thought you would die but even my expectations weren't this low");
-        $("#gameEnd").css("background-image", "url(images/enslaved.png)");
-
-        // Won
-    } else if (outcome[0] === "won") {
-        $("#gameEndResult").text("Somehow you won!");
-        $("#gameEndMessage").text("I didn't think you were capable but good job!");
-        $("#gameEnd").css("background-image", "url(images/won.png)");
-
-        // Wuss
-    } else if (outcome[0] === "wuss") {
-        $("#gameEndResult").text("You gave up?!");
-        $("#gameEndMessage").text("Seriously?! Wow, that is disappointing...you big wuss");
-        $("#gameEnd").css("background-image", "url(images/wuss.png)");
-    };
 
     // Set user score
     $("#userScore").text(newPlayer.score);
