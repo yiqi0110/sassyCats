95f19b686213fb1ab13e2aa344a157847ae6255c
Tim Lewis <timothydlewis@icloud.com>
2018-11-07 17:54:56 -0700

    Changed modal text and changed #planetMove opacity

--- public/js/planet.js
+++ public/js/planet.js
@@ -15,12 +15,14 @@ $(document).ready(function () {
         var freedom = Math.floor(Math.random() * 10 + 1);
         if (freedom <= 6) {
             // You escape
+            $("#prisonModal").find(".modal-title").text("Holy cat!");
             $("#prisonModal").find("#prisonBody").text("Beyond all odds you managed to slip past the guards and escape.  I can't believe it.  I didn't think you had any skills or worth as a cat.");
             $("#prisonModal").find("#die").hide();
             $("#prisonModal").find("#fight").hide();
             $("#prisonModal").find(".escape").show();
         } else {
             // You die in slavery
+            $("#prisonModal").find(".modal-title").text("Nope!");
             $("#prisonModal").find("#prisonBody").text("You were caught and the guards made an example out of you.  The other prisoners will think twice after seeing what happened to you.");
             $("#prisonModal").find("#fight").hide();
             $("#prisonModal").find("#die").show();
